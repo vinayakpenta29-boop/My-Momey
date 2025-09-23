c9e9718144f4b1b54e198a92ce30ff9e9e160d35
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-23 17:24:13 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/moneytracker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.moneytracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] titles = {"I Gave", "I Received", "Gave Summary", "Received Summary"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        TabAdapter adapter = new TabAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText(titles[position])).attach();
+    }
+}
