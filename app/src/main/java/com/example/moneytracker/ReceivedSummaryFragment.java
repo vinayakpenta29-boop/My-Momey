3a68b1eb80e1c43982661cd748e0174c60c7e492
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-23 17:33:53 +0530

    Create ReceivedSummaryFragment.java

---
+++ app/src/main/java/com/example/moneytracker/ReceivedSummaryFragment.java
@@ -0,0 +1,27 @@
+package com.example.moneytracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class ReceivedSummaryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_received_summary, container, false);
+        LinearLayout layout = v.findViewById(R.id.summaryLayout);
+        layout.removeAllViews();
+
+        for (String name : ReceivedFragment.receivedMap.keySet()) {
+            int amount = ReceivedFragment.receivedMap.get(name);
+            TextView tv = new TextView(getContext());
+            tv.setText(name + ": ₹" + amount);
+            layout.addView(tv);
+        }
+        return v;
+    }
+}
