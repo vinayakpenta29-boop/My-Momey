9de38eeab3cbb7a63be5c7a1acdab7f4dbd8e550
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-23 17:25:03 +0530

    Create TabAdapter.java

---
+++ app/src/main/java/com/example/moneytracker/TabAdapter.java
@@ -0,0 +1,30 @@
+package com.example.moneytracker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabAdapter extends FragmentStateAdapter {
+
+    public TabAdapter(FragmentActivity fa) {
+        super(fa);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: return new GivenFragment();
+            case 1: return new ReceivedFragment();
+            case 2: return new GivenSummaryFragment();
+            case 3: return new ReceivedSummaryFragment();
+            default: return new GivenFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
