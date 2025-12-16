973d94050e33f4831f6e752978aaabff2237fbc5
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-12-17 01:13:52 +0530

    Update BcUiHelper.java

--- app/src/main/java/com/example/moneytracker/BcUiHelper.java
+++ app/src/main/java/com/example/moneytracker/BcUiHelper.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.graphics.Color;
 
 import androidx.fragment.app.Fragment;
 
